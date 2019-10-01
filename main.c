75c1fe43783331cb4b5a695d69ef908c01fa1533
gangalal <38142529+gangalal@users.noreply.github.com>
2019-10-01 16:40:19 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -16,9 +16,30 @@
 
 #include <stdio.h>
 
-#define I2C0CONSET (*(volatile unsigned char *)(0x4001C000))
-#define I2C0CONCLR (*(volatile unsigned char *)(0x4001C018))
-#define I2C0DAT (*(volatile unsigned char *)(0x4001C008))
+#define PCONP (*(volatile unsigned int *)(0x400FC0C4))
+#define PCLKSEL0 (*(volatile unsigned int *)(0x400FC1A8))
+#define PINSEL1 (*(volatile unsigned int *)(0x4002C004))
+#define I2CPADCFG (*(volatile unsigned int *)(0x4002C07C))
+#define I2C0SCLH (*(volatile unsigned int *)(0x4001C010))
+#define I2C0SCLL (*(volatile unsigned int *)(0x4001C014))
+
+#define I2C0CONSET (*(volatile unsigned int *)(0x4001C000))
+#define I2C0CONCLR (*(volatile unsigned int *)(0x4001C018))
+#define I2C0DAT (*(volatile unsigned int *)(0x4001C008))
+
+const int tempReadAddr = 0x00;
+
+void I2CInit() {
+	PCONP |= (1 << 7); //resetting power control for I2C0
+	PCLKSEL0 &= ~(1 << 15) | (1 << 14); // resetting clock selection
+	PCLKSEL0 |= (0 << 15) | (0 << 14); //dividing CCLK by 8 *needs to review
+	I2C0SCLH = 5;
+	I2C0SCLL = 5;
+	PINSEL1 |= (1 << 22) | (0 << 23); //enabling SDA0 on pin p0.27
+	PINSEL1 |= (1 << 24) | (0 << 25); //enabling SCL0 on pin p0.28
+	//I2CPADCFG |= (0<<2); //configuring I2C0 register to standard mode
+	I2C0CONSET = (1 << 6);
+}
 
 void I2CStart() {
 	I2C0CONSET = (1 << 3); //set SI while configuring other bits
@@ -58,15 +79,26 @@ int I2CRead(int ack) {
 void I2CStop() {
 	I2C0CONSET = (1 << 4); //set STO to request stop condition
 	I2C0CONCLR = (1 << 3); //clear SI to active state machine
-	while (((I2C0CONSET >> 3) & 1) == 0) {
+	while (((I2C0CONSET >> 4) & 1) == 1) {
 		//wait for stop condition to be set
 	}
 }
 
+void writeToIO() {
+	I2CStart();
+	I2CWrite(0b01000000);
+	I2Cwrite(0x00);
+	I2CStop();
+}
+
 int main(void) {
-
+	I2CInit();
 	while (1) {
-
+		I2CStart();
+		printf("test\n");
+		I2CWrite(0b10011011);
+		printf("temperature sensor data %d\n", I2CRead(1));
+		I2CStop();
 	}
 	return 0;
 }
