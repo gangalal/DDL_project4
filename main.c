c131dc283d8707357d96f4d70197249be6066836
gangalal <38142529+gangalal@users.noreply.github.com>
2019-10-09 20:32:58 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  ===============================================================================
- Name        : project4.c
+ Name        : midterm.c
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
@@ -16,56 +16,38 @@
 
 #include <stdio.h>
 
-#define I2C0CONSET (*(volatile unsigned char *)(0x4001C000))
-#define I2C0CONCLR (*(volatile unsigned char *)(0x4001C018))
-#define I2C0DAT (*(volatile unsigned char *)(0x4001C008))
-
-void I2CStart() {
-	I2C0CONSET = (1 << 3); //set SI while configuring other bits
-	I2C0CONSET = (1 << 5); //set STA to start condition
-	I2C0CONCLR = (1 << 3); //clear SI to active state machine
-	while (((I2C0CONSET >> 3) & 1) == 0) {
-		//wait for start condition to be set
-	}
-	I2C0CONCLR = (1 << 5); //clear STA
-}
-
-void I2CWrite(int data) {
-	I2C0DAT = data;
-	I2C0CONCLR = (1 << 3); //clear SI to standard data
-	while (((I2C0CONSET >> 3) & 1) == 0) {
-		//wait for write condition to be set
-	}
-
-	//check I2C0STAT for ack or nak
-}
-
-int I2CRead(int ack) {
-	if (ack) {
-		I2C0CONSET = (1 << 2);
-	} else {
-		I2C0CONCLR = (1 << 2);
-	}
-	I2C0CONCLR = (1 << 2);
-	I2C0CONCLR = (1 << 3); //clear SI to start read
-	while (((I2C0CONSET >> 3) & 1) == 0) {
-		//wait for read condition to be set
-	}
-
-	return I2C0DAT;
-}
-
-void I2CStop() {
-	I2C0CONSET = (1 << 4); //set STO to request stop condition
-	I2C0CONCLR = (1 << 3); //clear SI to active state machine
-	while (((I2C0CONSET >> 3) & 1) == 0) {
-		//wait for stop condition to be set
-	}
-}
+#define FIO0DIR (*(volatile unsigned int *)0x2009C000)
+#define FIO0PIN (*(volatile unsigned int *)0x2009C014)
+#define PINMODE0 (*(volatile unsigned int *)0x4002C040) //remember if you use char * it won't work because we are shifting by bit 20 in line 35
 
 int main(void) {
 
+	FIO0DIR |= (1 << 4); // configure P0[4] as output
+	FIO0DIR |= (1 << 5); // configure P0[5] as output
+
+	FIO0DIR &= ~(1 << 10); // configure P0[10] as input
+	FIO0DIR &= ~(1 << 11); // configure P0[11] as input
+
+	FIO0PIN |= (1 << 4); //set P0[4] high
+	FIO0PIN &= ~(1 << 5); //set P0[5] low
+
+	//Pull-down resistor enabled
+	PINMODE0 |= (3 << 20); //you can also do PINMODE0 |= (1<<20)|(1<<21) // here 3 is 11 i.e shifting 1 on bit 20 and 21
+	//PINMODE0 |= (1<<20)| (1<<21);
+
 	while (1) {
+		/*if (((FIO0PIN >> 10) & 0x01) == 1) {
+			FIO0PIN &= ~(1 << 4); //set low
+		}
+
+		if (((FIO0PIN >> 11) & 0x01) == 0) {
+			FIO0PIN |= (1 << 5); //set high
+		}*/ //just to test
+
+		//problem c
+		if ((((FIO0PIN >> 10) & 0x01) == 1) && (((FIO0PIN >> 11) & 0x01) == 0)) {
+			FIO0PIN &= ~(1 << 4); //set low
+		}
 
 	}
 	return 0;
